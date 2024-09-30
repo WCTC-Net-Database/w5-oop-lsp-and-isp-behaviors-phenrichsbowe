2e2c98cf15ee53722b89017184ba76d5532bdace
phenrichsbowe <phenrichsbowe@my.wctc.edu>
2024-09-30 03:25:39 -0500

    feat(Ghost): implement fly action

--- Models/Ghost.cs
+++ Models/Ghost.cs
@@ -4,6 +4,7 @@ namespace W5_assignment_template.Models
 {
     public class Ghost(string Name) : IEntity, IFlyable
     {
+        private FlyAction? _flyAction;
         public string Name { get; set; } = Name;
 
         public void Attack(IEntity entity)
@@ -16,9 +17,13 @@ namespace W5_assignment_template.Models
             Console.WriteLine($"{Name} floats silently.");
         }
 
+        public void SetFlyAction(FlyAction flyAction) {
+            _flyAction = flyAction;
+        }
+
         public void Fly()
         {
-            Console.WriteLine($"{Name} flies rapidly through the air.");
+            _flyAction?.Invoke(Name);
         }
     }
 }

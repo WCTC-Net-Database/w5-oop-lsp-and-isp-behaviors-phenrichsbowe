fb372983ad6dbd7b1e83e40fa0639165b6cad9a7
phenrichsbowe <phenrichsbowe@my.wctc.edu>
2024-09-30 03:28:52 -0500

    feat(Program): archer and warlock models

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using W5_assignment_template.Interfaces;
-using W5_assignment_template.Models;
+﻿using W5_assignment_template.Models;
 using W5_assignment_template.Services;
 
 namespace W5_assignment_template
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var character = new Character();
-            var goblin = new Goblin();
-            var ghost = new Ghost();
+            Character character = new("Hero");
+            Goblin goblin = new("Goblin");
+            Ghost ghost = new("Ghost");
+            Archer archer = new("Archer");
+            Warlock warlock = new("Warlock");
 
-            var gameEngine = new GameEngine(character, goblin, ghost);
+            GameEngine gameEngine = new(character, goblin, ghost, archer, warlock);
             gameEngine?.Run();
         }
 

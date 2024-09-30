215e66526fc90354cfec6f56fc916b06ab0990f7
phenrichsbowe <phenrichsbowe@my.wctc.edu>
2024-09-30 03:28:19 -0500

    feat(GameEngine): archer and warlock models

--- Services/GameEngine.cs
+++ Services/GameEngine.cs
@@ -5,15 +5,23 @@ namespace W5_assignment_template.Services
 {
     public class GameEngine
     {
-        private readonly IEntity _character;
-        private readonly IEntity _goblin;
-        private readonly IEntity _ghost;
+        private readonly Character _character;
+        private readonly Goblin _goblin;
+        private readonly Ghost _ghost;
+        private readonly Archer _archer;
+        private readonly Warlock _warlock;
 
-        public GameEngine(IEntity character, IEntity goblin, IEntity ghost)
+        public GameEngine(Character character, Goblin goblin, Ghost ghost, Archer archer, Warlock warlock)
         {
             _character = character;
+            
             _goblin = goblin;
+
             _ghost = ghost;
+            _ghost.SetFlyAction(name => Console.WriteLine($"{name} flies through the air raipdly!"));
+
+            _archer = archer;
+            _warlock = warlock;
         }
 
         public void Run()
@@ -30,7 +38,25 @@ namespace W5_assignment_template.Services
 
             _ghost.Move();
             _ghost.Attack(_character);
-            ((Ghost) _ghost).Fly();
+
+            if (_ghost is IFlyable) {
+                _ghost.Fly();
+            }
+
+            _archer.Move();
+            _archer.Attack(_goblin);
+
+            if (_archer is IShootable) {
+                _archer.Shoot();
+            }
+
+            _warlock.Move();
+            _warlock.Attack(_ghost);
+
+            if (_warlock is ICastable) {
+                _warlock.Cast("healing");
+                _warlock.Cast("protection");
+            }
         }
     }
 }

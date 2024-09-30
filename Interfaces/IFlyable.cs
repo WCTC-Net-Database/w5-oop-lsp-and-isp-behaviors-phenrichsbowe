4f9e37696dd5650be590b68769e6c12c9b01c0be
phenrichsbowe <phenrichsbowe@my.wctc.edu>
2024-09-30 03:24:59 -0500

    feat(IFlyable): delegate for creating custom fly behavior

--- Interfaces/IFlyable.cs
+++ Interfaces/IFlyable.cs
@@ -1,5 +1,7 @@
 namespace W5_assignment_template.Interfaces;
 
+public delegate void FlyAction(string entityName);
 public interface IFlyable {
+  public void SetFlyAction(FlyAction flyAction);
   public void Fly();
 }

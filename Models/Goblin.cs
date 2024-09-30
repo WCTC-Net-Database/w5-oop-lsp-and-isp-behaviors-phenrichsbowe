214511efdcb9ea98c39f11e06299aed90445fca5
phenrichsbowe <phenrichsbowe@my.wctc.edu>
2024-09-30 03:26:25 -0500

    tweak(Goblin): remove fly method

--- Models/Goblin.cs
+++ Models/Goblin.cs
@@ -2,18 +2,13 @@
 
 namespace W5_assignment_template.Models
 {
-    public class Goblin : IEntity
+    public class Goblin(string Name) : IEntity
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = Name;
 
         public void Attack(IEntity target)
         {
             Console.WriteLine($"{Name} attacks {target.Name} with a terrifying shout.");
-        }
-
-        public void Fly()
-        {
-            throw new NotImplementedException();
         }
 
         public void Move()
